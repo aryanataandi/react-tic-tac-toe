108d33354a5f928ec61e54e8dcbe4a287bd2c456
aryanataandi <a.ryan.ata.ap@gmail.com>
2024-04-21 20:00:02 +0700

    update player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,30 @@
+import { useState } from 'react';
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
+
+  const handleClick = () => {
+    setIsEditing((editing) => !editing);
+  };
+
+  const handleChange = (event) => {
+    setPlayerName(event.target.value);
+  };
+
+  let playerNameField = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    playerNameField = <input type="text" className="player-name" value={playerName} onChange={handleChange} required />;
+  }
+
   return (
     <li>
       <span className="player">
-      <span className="player-name">{name}</span>
+        {playerNameField}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleClick}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
   );
 }
